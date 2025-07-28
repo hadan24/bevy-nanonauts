d428744df733d87819e26480d603986e9f6728e8
hadan24 <ha.dan.t02@gmail.com>
2025-07-27 23:41:07 -0700

    hello world + more preliminary stuff

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub use bevy::prelude::*;
+
+// for faster iteration, from https://taintedcoders.com/bevy/windows
+pub fn close_on_esc(
+    mut commands: Commands,
+    windows: Query<(Entity, &Window)>,
+    input: Res<ButtonInput<KeyCode>>
+) {
+    // Query to get window Entity + its Window component
+    // only close on esc if focused
+    for (window, properties) in windows {
+        if properties.focused && input.just_pressed(KeyCode::Escape) {
+            commands.entity(window).despawn();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use bevy::prelude::*;
+use bevy_nanonauts::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, || {println!("hello world!")})
+        .add_systems(Update, close_on_esc)
         .run();
 }
