c8dc2c1a42b829649b24344b1f09e2a8d537a7df
hadan24 <ha.dan.t02@gmail.com>
2025-11-11 16:58:10 -0800

    Fixed nanonaut sprites to render consistently w/ same code

--- src/nanonaut.rs
+++ src/nanonaut.rs
@@ -7,8 +7,8 @@ use crate::animation;
 - collider
 - HP
 */
-const NANONAUT_WIDTH: u32 = 181;
-const NANONAUT_HEIGHT: u32 = 229;
+const NANONAUT_WIDTH: u32 = 148;
+const NANONAUT_HEIGHT: u32 = 200;
 // puts nanonaut on ground level, offset by 1/2 height bc centers are origins
 const NANONAUT_GROUND_LEVEL: f32 = crate::GROUND_LEVEL + ((NANONAUT_HEIGHT/2) as f32);
 
@@ -30,8 +30,8 @@ pub fn spawn_nanonaut(
     );
     let anim_frames = animation::AnimationFrames { first: 0, last: 6 };
 
-    // offset by -400 bc center is origin, put nanonaut on left quarter of window
-    let nanonaut_x = -400.0 + (crate::WINDOW_WIDTH as f32)*0.25;
+    // put nanonaut on left quarter of window
+    let nanonaut_x = -(crate::WINDOW_WIDTH as f32)/2.0 + (crate::WINDOW_WIDTH as f32)*0.25;
 
     commands.spawn((
         Nanonaut,
@@ -41,7 +41,7 @@ pub fn spawn_nanonaut(
         ),
         Transform::from_xyz(nanonaut_x, NANONAUT_GROUND_LEVEL, 1.0),
         anim_frames,
-        animation::AnimationTimer(Timer::from_seconds(0.09, TimerMode::Repeating))
+        animation::AnimationTimer(Timer::from_seconds(0.08, TimerMode::Repeating))
     ));
 }
 
